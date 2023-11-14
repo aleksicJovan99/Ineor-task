1ab4910c26555098805fc186fefe94215075d400
Jovan Aleksić <jovan.aleksic@netica.si>
2023-11-14 12:22:42 +0100

    added  MoviesControllerTest

---
+++ Tests/MockServices/MovieServiceMock.cs
@@ -0,0 +1,74 @@
+﻿using Contracts;
+using Entities;
+
+namespace Tests;
+public class MovieServiceMock : IMovieService
+{
+    private readonly IEnumerable<MovieDto> _movies;
+    public MovieServiceMock()
+    {
+        _movies = new List<MovieDto> 
+        {
+            new MovieDto 
+            {
+                Id=1, 
+                Name="Movie1", 
+                Rating=10, 
+                ReleaseDate=DateTime.Now, 
+                DirectorName="Director1"
+            },
+            new MovieDto 
+            {
+                Id=2, 
+                Name="Movie2", 
+                Rating=8, 
+                ReleaseDate=DateTime.Now, 
+                DirectorName="Director2"
+            },
+            new MovieDto 
+            {
+                Id=3, 
+                Name="Movie3", 
+                Rating=7, 
+                ReleaseDate=DateTime.Now, 
+                DirectorName="Director3"
+            },
+
+        };
+    }
+
+    public Task<MovieDto> CreateMovie(MovieForCreationDto movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<MovieDto>> CreateMovieCollection(IEnumerable<MovieForCreationDto> movieCollection)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteMovie(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<MovieDto> GetMovie(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<MovieDto>> GetMovies()
+    {
+        return Task.FromResult<IEnumerable<MovieDto>>(_movies);
+    }
+
+    public Task<IEnumerable<MovieDto>> GetMoviesPaging(MovieParameters movieParameters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateMovie(int id, MovieForUpdateDto movie)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Tests/MoviesControllerTest.cs
@@ -0,0 +1,60 @@
+﻿using Castle.Components.DictionaryAdapter.Xml;
+using Contracts;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using MovieApp;
+
+namespace Tests;
+public class MoviesControllerTest
+{
+    private MoviesController _controller;
+    private Mock<IMovieService> _service;
+
+    [SetUp]
+    public void SetUp() 
+    {
+        _service = new Mock<IMovieService>();
+        _controller = new MoviesController(_service.Object);
+            
+    }
+
+    [Test]
+    public void GetMovies_ShouldReturnOkWithMoviesDtoList()
+    {
+        _service.Setup(s => s.GetMovies())
+        .Returns(Task.FromResult(
+            new List<MovieDto>() { new(){Id=1}, new(){Id=2} }
+            .AsEnumerable()));
+
+        var result = _controller.GetMovies().Result as ObjectResult;
+
+        Assert.IsNotNull(result);
+        Assert.That(result.StatusCode, Is.EqualTo(200));
+
+        var movies = result.Value as IEnumerable<MovieDto>;
+
+        Assert.IsNotNull(movies);
+        Assert.That(movies.ToList().Count, Is.EqualTo(2));
+    }
+
+    [Test]
+    public  void GetMovie_Id_ShouldReturnOkWithMovieDto()
+    {
+        int id = 1;
+        _service.Setup(s => s.GetMovie(id))
+        .Returns(Task.FromResult(
+            new MovieDto() {Id=1} ));
+
+        var result = _controller.GetMovie(id).Result as ObjectResult;
+
+        Assert.IsNotNull(result);
+        Assert.That(result.StatusCode, Is.EqualTo(200));
+
+        var movie = result.Value as MovieDto;
+        Assert.IsNotNull(movie);
+    }
+
+
+
+}
